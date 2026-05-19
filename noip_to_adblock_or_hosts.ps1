534a4c506974d9050e22d29e70808859d8c19829
pewbert <mercenary.kid@gmail.com>
2026-05-18 23:09:20 -0400

    Changes to be committed: 	modified:   noip_to_adblock_or_hosts.ps1 	modified:   raw_url_paths.txt 	new file:   sorted_adblock.blocklist

--- noip_to_adblock_or_hosts.ps1
+++ noip_to_adblock_or_hosts.ps1
@@ -12,31 +12,31 @@ param (
 $ipaddr ='\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b' #regex for a valid ipv4 address
 #create the raw list of hostnames from url file
 if ($url) {
-    $files = Get-Content -Path ".\$url"
+    $files = Get-Content -Path "$url"
     foreach ($f in $files) {
-        Invoke-WebRequest -uri "$f" | Add-Content -Path ".\$list"
+        Invoke-WebRequest -uri "$f" | Add-Content -Path ".\list.txt"
     }
-    $list = ".\$list"
+    $list = Get-ChildItem -Path ".\list.txt" | Select-Object -ExpandProperty Name
 }
-else {
-    if (-not (Test-Path -Path "$list")) {
-        Write-Host "File $list does not exist. Please check the path and try again."
-        exit
-    }
-    else {
-        $tmp = Get-Content -Path "$list" | Select-String -pattern '(#)|([)|(])' -notmatch  #strip all comments and bracket blocks from entire file
+if ($list) {
+        $tmp = Get-Content -Path ".\$list" | Select-String -pattern '(#)|([)|(])|(!)' -notmatch  #strip all comments and bracket blocks from entire file
         #remove the unnecessary crap and format the list for adblock-plus or hosts file format.
         if ($filter -eq "adblock") {
             $unsorted = $tmp -replace $ipaddr,"" -replace '(^\s+|\s+$)','' -replace '\s+','' | ForEach-Object{"||$_^"} #remove spaces and ip addresses, change format to adblock-plus eg. ||domain.example.com^ 
-            $unsorted | sort-object -Unique > ".\$out_file" #sorted and duplicates removed
+            $unsorted | sort-object -Unique > $out_file #sorted and duplicates removed
         }
         elseif ($filter -eq "hosts") {
             $unsorted = $tmp -replace $ipaddr,"" -replace '(^\s+|\s+$)','' -replace '\s+','' | ForEach-Object{"0.0.0.0 $_"} #remove spaces and ip addresses, change format to /etc/hosts file format
-            $unsorted | sort-object -Unique > ".\$out_file" #sorted and duplicates removed
+            $unsorted | sort-object -Unique > $out_file #sorted and duplicates removed
         }
         else {
             Write-Host "Invalid filter option. Please choose either 'adblock' or 'hosts' and try again."
             exit
         }
-    }       <# Action when all if and elseif conditions are false #>
 }
+elseif (-not (Test-Path -Path ".\$list") ) {
+        Write-Host "File list: $list does not exist. Please check the path and try again."
+        exit
+        }
+else { write-host "wtf"} 
+
